81e2664d5ef05b945c698ebd2349d518549ae023
dooster <jisbender@gmail.com>
2016-03-07 21:22:18 -0500

    Add Home and Jeremy's Recommendations buttons.
    Wire the Home, Jeremy's Recommendations, and My Saved Locations buttons up through the ViewModel. On click, each button emptys the current mapLocations array and then repopulates it with their own data.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+var jeremyLocations = [
+	'test', 'poop'
+];
+
+var savedLocations = [
+	//put this inside the ViewModel???
+];
+
+var results = [
+	'hi', 'ho'
+];
+
 function initMap() {
 	var map;
 	var mapOptions = {
@@ -16,6 +28,27 @@ var ViewModel = function () {
 	var self=this;
 
 	this.mapLocations = ko.observableArray(['test', 'two', 'three', 'four']);
+
+	self.displayResults = function() {
+		this.mapLocations.removeAll();
+		results.forEach(function(item) {
+			self.mapLocations.push(item)
+		});
+	};
+
+	self.displayJerLoc = function() {
+		this.mapLocations.removeAll();
+		jeremyLocations.forEach(function(item) {
+			self.mapLocations.push(item)
+		});
+	};
+
+	self.displayMyLoc = function() {
+		this.mapLocations.removeAll();
+		savedLocations.forEach(function(item){
+			self.mapLocations.push(item)
+		});
+	};
 };
 
 ko.applyBindings(new ViewModel());
