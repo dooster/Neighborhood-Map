0f96f600be0d52f1192522857b5d48bb72b6be9c
dooster <jisbender@gmail.com>
2016-03-09 07:45:57 -0500

    Move the FourSquare AJAX request into the ViewModel.
    I can now push results from the AJAX request straight into the mapLocations observableArray. But working out how to retrieve those results again after clicking into other menus.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,3 @@
-var getFourSquare = (function() {
-	var config = {
-		clientId: 'Y2I0LGBKFU2FCQLC3XOQEH4CIV1R4IKT4X10CT1FPAZS2VYC',
-		clientSecret: 'HVHDQBVKYMWSV5Q45AH44VAFMRF1D5TTE41J41YRYGAZ253R',
-		apiUrl: 'https://api.foursquare.com/'
-	};
-
-	$.ajax({
-		url: config.apiUrl + 'v2/venues/explore',
-		dataType: 'json',
-		data: 'll=40.7652299,-73.9187454&client_id=' + config.clientId + '&client_secret=' + config.clientSecret + '&v=20140806&m=foursquare',
-		async: true
-		}).done(function(data) {
-			venue = data.response.groups[0].items;
-			for (var i = 0; i < venue.length; i++){
-				var venues = venue[i].venue.name;
-				results.push(venues);
-			}
-		});
-})();
-
-
 var jeremyLocations = [
 	'test', 'poop'
 ];
@@ -47,11 +25,13 @@ function initMap() {
 var ViewModel = function () {
 	var self=this;
 
-	this.mapLocations = ko.observableArray(['test', 'two', 'three', 'four']);
+	this.fourSquareLocations = [];
+
+	this.mapLocations = ko.observableArray();
 
 	self.displayResults = function() {
 		this.mapLocations.removeAll();
-		results.forEach(function(item) {
+		self.fourSquareLocations.forEach(function(item) {
 			self.mapLocations.push(item)
 		});
 	};
@@ -69,6 +49,31 @@ var ViewModel = function () {
 			self.mapLocations.push(item)
 		});
 	};
+
+	self.getFourSquare = (function() {
+		var config = {
+			clientId: 'Y2I0LGBKFU2FCQLC3XOQEH4CIV1R4IKT4X10CT1FPAZS2VYC',
+			clientSecret: 'HVHDQBVKYMWSV5Q45AH44VAFMRF1D5TTE41J41YRYGAZ253R',
+			apiUrl: 'https://api.foursquare.com/'
+		};
+
+		$.ajax({
+			url: config.apiUrl + 'v2/venues/explore',
+			dataType: 'json',
+			data: 'll=40.7652299,-73.9187454&client_id=' +
+				config.clientId +
+				'&client_secret=' +
+				config.clientSecret +
+				'&v=20140806&m=foursquare',
+			async: true
+			}).done(function(data) {
+				venue = data.response.groups[0].items;
+				for (var i = 0; i < venue.length; i++){
+					var venues = venue[i].venue.name;
+					self.fourSquareLocations.push(venues);
+				}
+			});
+	})();
 };
 
 ko.applyBindings(new ViewModel());
